0c4487b7af735061712840a3d6eb8d01ed6d0334
Smaelovic <smaelcv@gmail.com>
2023-02-26 14:06:24 +0100

    Updates

--- index.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="front_end\ecom_store\build\static\css\" />
-<div id="root"></div>
-<!-- <img src="front_end\ecom_store\build\static\media\logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg" /> -->
-<script src="front_end\ecom_store\build\static\js\"></script>
